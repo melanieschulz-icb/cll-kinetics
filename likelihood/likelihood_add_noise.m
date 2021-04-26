155088f46d916bcd2c55d0cacdacf4ad76057127
melanieschulz-icb <73172648+melanieschulz-icb@users.noreply.github.com>
2021-04-26 11:21:00 +0200

    Delete likelihood directory

--- likelihood/likelihood_add_noise.m
+++
@@ -1,3 +0,0 @@
-function [likelihood]=likelihood_add_noise(par,xdata,ydata,zdatanorm,tspanX, tspanY,tspanZ, model, refill,seperate_sd)
-    likelihood = exp(-0.5*diff_integral_summed_sd(par,xdata,ydata,zdatanorm,tspanX, tspanY,tspanZ, model,refill, seperate_sd));
-end
