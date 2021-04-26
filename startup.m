41b5716d09dc5de43cdf480f32e594e93b15154f
melanieschulz-icb <73172648+melanieschulz-icb@users.noreply.github.com>
2021-04-26 17:20:29 +0200

    Delete startup.m

--- startup.m
+++
@@ -1,7 +0,0 @@
-addpath(genpath('ObjectiveFunctions'));
-addpath(genpath('Helpfunctions'));
-addpath(genpath('Solutions'));
-addpath(genpath('likelihood'));
-addpath(genpath('AnalyseResults'));
-addpath(genpath('Clustering'));
-addpath(genpath('mixedEffectModeling'));
